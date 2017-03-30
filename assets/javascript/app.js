872b447e5f806a6d8a4cec4527c373c98e69feed
Gracie Sullivan <gracesullivan@Gracies-MacBook-Pro.local>
2017-03-30 16:42:13 -0400

    final product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@
              // creating url to search Giphy for video games
 
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=10";
-            console.log(queryURL);
+
 
           // e.preventDefault(e);
           // console.log("hello");
@@ -23,20 +23,31 @@
             url: queryURL,
             method: "GET"
             }) .done(function(response) {
+              console.log(response);
 
                 //creating a variable for the repsonse data
 
                 var results = response.data;
 
-
                 //looping through reponse data
 
                 for ( var i = 0 ; i < results.length; i++ ){
 
+                   // Only taking action if the photo has an appropriate rating
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+
+                     // Creating a div with the class "item"
+                      var gifDiv = $("<div class='item'>");
+
+                     // Storing the result item's rating
+                    var rating = results[i].rating;
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
                    //retrieving the urls for the gifs
 
-                    var gifURL = results[i].bitly_gif_url;
-                    console.log(gifURL);
+                    var gifURL = results[i].images.fixed_height.url;
 
                     //creating a div to hold the gifs
 
@@ -49,16 +60,17 @@
                     //appending the image to the game div
 
                     gameDiv.append(image);
+                    gameDiv.append(p);
 
                     //prepend that gif the above the other gifs
 
-                    $("#game").prepend(gameDiv);
+                    $("#games").prepend(gameDiv);
 
                 }
-
-              });
+              }
             });
-          }
+          });
+        }
 
       //create a function with a for loop that creates buttons based on input when the game button is clicked
 
@@ -122,7 +134,8 @@
 
       //add a click event listner to all elements with the class of newButton
 
-      $(document).on("click", ".newButton", displayGif);{
+      $(document).on("click", ".newButton", ".gameglitch", displayGif);{
 
         console.log('test');
       }
+
