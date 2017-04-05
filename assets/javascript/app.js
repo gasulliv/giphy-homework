83fedc75488475c8c4fc1610f8249da33cd3717d
Gracie Sullivan <gracesullivan@wifi_guest-10-38-98-21.uncc.edu>
2017-04-04 20:13:14 -0400

    updated javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,141 +1,140 @@
      // Initial array of games
-      var games = ["bioshock", "fallout", "skyrim", "bloodborne"];
+     var games = ["bioshock", "fallout", "skyrim", "bloodborne"];
 
-        //displayGif function re-renders the HTML to display the gifs
+     //displayGif function re-renders the HTML to display the gifs
 
-        function displayGif(){
+     function displayGif() {
 
-            $(".newButton").on("click", function(){
-            //creating the game variable
-            // In this case, the "this" keyword refers to the button that was clicked
-            var game = $(this).attr("game-name");
+         $(".newButton").on("click", function() {
+             //creating the game variable
+             // In this case, the "this" keyword refers to the button that was clicked
+             var game = $(this).attr("game-name");
 
              // creating url to search Giphy for video games
 
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=10";
+             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + game + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 
-          // e.preventDefault(e);
-          // console.log("hello");
+             // e.preventDefault(e);
+             // console.log("hello");
 
-            // Performing our AJAX GET request
-            $.ajax({
-            url: queryURL,
-            method: "GET"
-            }) .done(function(response) {
-              console.log(response);
+             // Performing our AJAX GET request
+             $.ajax({
+                 url: queryURL,
+                 method: "GET"
+             }).done(function(response) {
+                 console.log(response);
 
-                //creating a variable for the repsonse data
+                 //creating a variable for the repsonse data
 
-                var results = response.data;
+                 var results = response.data;
 
-                //looping through reponse data
+                 //looping through reponse data
 
-                for ( var i = 0 ; i < results.length; i++ ){
+                 for (var i = 0; i < results.length; i++) {
 
-                   // Only taking action if the photo has an appropriate rating
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                     // Only taking action if the photo has an appropriate rating
+                     if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-                     // Creating a div with the class "item"
-                      var gifDiv = $("<div class='item'>");
+                         // Creating a div with the class "item"
+                         var gifDiv = $("<div class='item'>");
 
-                     // Storing the result item's rating
-                    var rating = results[i].rating;
+                         // Storing the result item's rating
+                         var rating = results[i].rating;
 
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + rating);
+                         // Creating a paragraph tag with the result item's rating
+                         var p = $("<p>").text("Rating: " + rating);
 
-                   //retrieving the urls for the gifs
+                         //retrieving the urls for the gifs
 
-                    var gifURL = results[i].images.fixed_height.url;
+                         var gifURL = results[i].images.fixed_height.url;
 
-                    //creating a div to hold the gifs
+                         //creating a div to hold the gifs
 
-                    var gameDiv = $("<div class = 'gameglitch'>");
+                         var gameDiv = $("<div class = 'gameglitch'>");
 
-                    //creating an element to hold the gifs
+                         //creating an element to hold the gifs
 
-                    var image = $("<img>").attr("src", gifURL);
+                         var image = $("<img>").attr("src", gifURL);
 
-                    //appending the image to the game div
+                         //appending the image to the game div
 
-                    gameDiv.append(image);
-                    gameDiv.append(p);
+                         gameDiv.append(image);
+                         gameDiv.append(p);
 
-                    //prepend that gif the above the other gifs
+                         //prepend that gif the above the other gifs
 
-                    $("#games").prepend(gameDiv);
+                         $("#games").prepend(gameDiv);
 
-                }
-              }
-            });
-          });
-        }
+                     }
+                 }
+             });
+         });
+     }
 
-      //create a function with a for loop that creates buttons based on input when the game button is clicked
+     //create a function with a for loop that creates buttons based on input when the game button is clicked
 
-        function createButton(){
+     function createButton() {
 
-            //deletes the other games prior to adding new ones
+         //deletes the other games prior to adding new ones
 
-            $("#glitchbutton").empty();
+         $("#glitchbutton").empty();
 
-           //necessary or we will need to repeat buttons
+         //necessary or we will need to repeat buttons
 
-            //looping through
+         //looping through
 
-            for (var i = 0; i < games.length; i++){
+         for (var i = 0; i < games.length; i++) {
 
-            //creating the button html
+             //creating the button html
 
-            var g = $("<button>");
+             var g = $("<button>");
 
-          //add a class of newButton to the button
+             //add a class of newButton to the button
 
-            g.addClass("newButton");
+             g.addClass("newButton");
 
-            //add a game-attribute
+             //add a game-attribute
 
-            g.attr("game-name", games[i]);
+             g.attr("game-name", games[i]);
 
-            //Providing the initial button text
+             //Providing the initial button text
 
-            g.text(games[i]);
+             g.text(games[i]);
 
-            //Adding the button to the buttons div
+             //Adding the button to the buttons div
 
-            $("#glitchbutton").append(g);
-        }
-      }
+             $("#glitchbutton").append(g);
+         }
+     }
 
-      //the on click function that handles events where a game button is clicked
+     //the on click function that handles events where a game button is clicked
 
-       $("#addGame").on("click", function(event) {
+     $("#addGame").on("click", function(event) {
 
-          event.preventDefault();
+         event.preventDefault();
 
-          //grabbing input from textbox
+         //grabbing input from textbox
 
-          var game = $("#game-input").val().trim();
+         var game = $("#game-input").val().trim();
 
          //adding the game from the textbox to our array
 
-          games.push(game);
+         games.push(game);
 
-        //Calling the createButton function
+         //Calling the createButton function
 
-          createButton();
+         createButton();
 
-        //calling the displayGif function
+         //calling the displayGif function
 
-          displayGif();
+         displayGif();
 
-      });
+     });
 
-      //add a click event listner to all elements with the class of newButton
+     //add a click event listner to all elements with the class of newButton
 
-      $(document).on("click", ".newButton", ".gameglitch", displayGif);{
+     $(document).on("click", ".newButton", ".gameglitch", displayGif); {
 
-        console.log('test');
-      }
-
+         console.log('test');
+     }
